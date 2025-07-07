a90aab39a2a5db969c0f6bc9a3614d607717cc7a
Deborah Olagunju <olagunjudeborah2007@gmail.com>
2025-07-07 00:19:23 +0000

    update

--- myfolder/docs/simple_script.sh
+++ myfolder/docs/simple_script.sh
@@ -10,14 +10,15 @@ echo "You are in: $(pwd)"
 
 # test to check if a folder exists exists
 
-folder="images"
+folder="/home/debbie/Linux-commands-showcase/myfolder/mydocs/images"
 
 if [ -d "$folder" ]; then 
 	echo " The folder '$folder' is in this directory. "
 	echo "Here are the files in this folder"
 	
-	cd "$folder"
+	cd "$(dirname "$0")"
 	ls
+	echo "Script ran at $(date)" >> test_log.txt
 else
 	echo "The folder '$folder' is not in this directory. "
 
